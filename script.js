883828236ccc0d6551f0a50d88bcb18039cfb220
saidurga2002 <128450214+saidurga2002@users.noreply.github.com>
2025-08-21 14:07:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const message = document.createElement("p");
+  message.textContent = "Thanks for visiting my portfolio!";
+  document.body.appendChild(message);
+});
